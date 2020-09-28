40aed2383f39c65334932cd92cf945b9ada0f1ac
unknown <jkvapil6@gmail.com>
2020-09-28 19:47:01 +0200

    Updates

---
+++ precache-manifest.6647b6adc5e9c5386a2ceb8703b85467.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1370d01195cedc8ff1f464f9044a82a5",
+    "url": "/unityFirst/index.html"
+  },
+  {
+    "revision": "c7bf5bd436237a5ac18d",
+    "url": "/unityFirst/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "872af766e69797563e2a",
+    "url": "/unityFirst/static/js/2.a099d96e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/unityFirst/static/js/2.a099d96e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7bf5bd436237a5ac18d",
+    "url": "/unityFirst/static/js/main.7802e772.chunk.js"
+  },
+  {
+    "revision": "204f0fa095006f90a149",
+    "url": "/unityFirst/static/js/runtime-main.f39e15e6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/unityFirst/static/media/logo.5d5d9eef.svg"
+  }
+]);
